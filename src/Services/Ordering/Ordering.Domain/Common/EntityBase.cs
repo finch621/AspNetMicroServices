aa42186c5d6b5b032b70d6e60f97f86d4bd9c145
doffi <yongco.doffinyl@gmail.com>
2023-10-15 10:26:17 +0800

    ordering domain

---
+++ src/Services/Ordering/Ordering.Domain/Common/EntityBase.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace Ordering.Domain.Common;
+public enum LastModifiedStatusEnum
+{
+    Deleted,
+    Created,
+    Updated,
+}
+
+public abstract class EntityBase
+{
+    public int Id { get; protected set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+    public DateTime DeletedAt { get; set; }
+    public string LastModifiedBy { get; set; }
+    public LastModifiedStatusEnum LastModifiedStatus { get; set; }
+}
